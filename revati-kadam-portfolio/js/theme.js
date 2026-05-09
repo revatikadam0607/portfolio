6689c82cfc0d3b3d604b4095bed5c4a25a31b604
Shayan <shayan.bhowmik.pune@gmail.com>
2026-05-09 19:26:18 +0530

    fixed the light theme issue

--- revati-kadam-portfolio/js/theme.js
+++ revati-kadam-portfolio/js/theme.js
@@ -3,9 +3,15 @@ function setTheme(mode){
     document.documentElement.style.setProperty('--bg','#f8fafc');
     document.documentElement.style.setProperty('--text','#111');
     document.documentElement.style.setProperty('--card','#e2e8f0');
+    document.documentElement.style.setProperty('--sidebar','#e2e8f0');
+    document.documentElement.style.setProperty('--btn','#f1f5f9');
+    document.documentElement.style.setProperty('--secondary-bg','#cbd5e1');
   } else {
     document.documentElement.style.setProperty('--bg','#0f172a');
     document.documentElement.style.setProperty('--text','#e5e7eb');
     document.documentElement.style.setProperty('--card','#1e293b');
+    document.documentElement.style.setProperty('--sidebar','#020617');
+    document.documentElement.style.setProperty('--btn','#333');
+    document.documentElement.style.setProperty('--secondary-bg','#1e293b');
   }
 }
